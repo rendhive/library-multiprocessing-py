5e3b0aba591f5198d213e4c034d76af65ee75939
rendhive <rendhivecode@gmail.com>
2025-12-10 16:21:41 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul multiprocessing.py

---
+++ 1. Mengimpor Modul multiprocessing.py
@@ -0,0 +1,5 @@
+import multiprocessing
+
+print("Modul multiprocessing berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menggunakan proses paralel dengan multiprocessing di Python.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari multiprocessing.
